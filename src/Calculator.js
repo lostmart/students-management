681d0c9b443d4c653085386b6e62fa1d1512b961
lostmart <lostmart@hotmail.com>
2024-11-17 17:00:41 +0100

    describe cases

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,6 +2,16 @@ class Calculator {
 	add(a, b) {
 		return a + b
 	}
+	subtract(a, b) {
+		return a - b
+	}
+	divide(a, b) {
+		if (b === 0) throw new Error("Can't divide by 0")
+		return a / b
+	}
+	multiply(a, b) {
+		return a * b
+	}
 }
 
 module.exports = Calculator
--- tests/Calculator.test.js
+++ tests/Calculator.test.js
@@ -3,9 +3,45 @@ const Calculator = require("../src/Calculator")
 const { expect } = chai
 
 describe("Calculator", () => {
-	const calculatorService = new Calculator()
-	const result = calculatorService.add(1, 2)
-	it("should return the sum of a and b", () => {
-		expect(result).to.equal(3)
+	let calculatorService
+
+	// Initialize a new Calculator instance before each test
+	beforeEach(() => {
+		calculatorService = new Calculator()
+	})
+
+	describe("add", () => {
+		it("should return the sum of a and b", () => {
+			const result = calculatorService.add(1, 2)
+			expect(result).to.equal(3)
+		})
+	})
+
+	describe("subtract", () => {
+		it("should return the subtraction of a and b", () => {
+			const result = calculatorService.subtract(3, 2)
+			expect(result).to.equal(1)
+		})
+	})
+
+	describe("divide", () => {
+		it("should return the division of a and b", () => {
+			const result = calculatorService.divide(16, 4)
+			expect(result).to.equal(4)
+		})
+
+		it("should throw an error when dividing by 0", () => {
+			expect(() => calculatorService.divide(10, 0)).to.throw(
+				Error,
+				"Can't divide by 0"
+			)
+		})
+	})
+
+	describe("multiply", () => {
+		it("should return the multiplication of a and b", () => {
+			const result = calculatorService.multiply(4, 4)
+			expect(result).to.equal(16)
+		})
 	})
 })
